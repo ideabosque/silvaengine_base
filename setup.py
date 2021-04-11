b017926a55b0e778b698adc1de1c06b6ef4c2232
ideabosque <ideabosque@gmail.com>
2021-04-10 23:52:22 -0700

    Apply black formatter.

--- setup.py
+++ setup.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-__author__ = 'bibow'
+__author__ = "bibow"
 
 from setuptools import find_packages, setup
 
 setup(
-    name='SilvaEngine-Base',
-    version='0.0.1',
-    author='Idea Bosque',
-    author_email='ideabosque@gmail.com',
-    description='SilvaEngine Base',
+    name="SilvaEngine-Base",
+    version="0.0.1",
+    author="Idea Bosque",
+    author_email="ideabosque@gmail.com",
+    description="SilvaEngine Base",
     long_description=__doc__,
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    platforms='Linux',
-    install_requires=['SilvaEngine-Utility'],
+    platforms="Linux",
+    install_requires=["SilvaEngine-Utility"],
     classifiers=[
-        'Programming Language :: Python',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-    ]
+        "Programming Language :: Python",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
 )
