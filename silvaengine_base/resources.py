237ab0dd0ac6766fa4a5c32f991f32d467c4270e
ideabosque <ideabosque@gmail.com>
2021-04-17 21:05:05 -0700

    Added pynamodb to manage core tables.

---
+++ silvaengine_base/resources.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+__author__ = "bibow"
+
+import json, traceback, os
+from .lambdabase import LambdaBase
+from .models import BaseModel
+from silvaengine_utility import Utility
+
+
+class Resources(LambdaBase):
+    def __init__(self, logger):  # implementation-specific args and/or kwargs
+        # implementation
+        self.logger = logger
+        BaseModel.Meta.region = os.environ["REGIONNAME"]
+
+    def handle(self, event, context):
+        # TODO implement
+        try:
+            area = event["pathParameters"]["area"]
+            method = event["httpMethod"]
+            endpoint_id = event["pathParameters"]["endpoint_id"]
+            funct = event["pathParameters"]["proxy"]
+            params = dict(
+                {"endpoint_id": endpoint_id, "area": area},
+                **(
+                    event["queryStringParameters"]
+                    if event["queryStringParameters"] is not None
+                    else {}
+                ),
+            )
+            body = event["body"]
+            api_key = event["requestContext"]["identity"]["apiKey"]
+            self.logger.info([endpoint_id, funct, api_key, method])
+
+            (setting, function) = LambdaBase.get_function(
+                endpoint_id, funct, api_key=api_key, method=method
+            )
+
+            assert (
+                area == function.area
+            ), f"Area ({area}) is not matched the configuration of the function ({funct}).  Please check the parameters."
+
+            # If auth_required is True, validate authorization.
+            # If graphql, append the graphql query path to the path.
+            user = event["requestContext"]["identity"].get("user")
+            assert (
+                True if function.config.auth_required else True
+            ), f"Don't have the permission to access at /{area}/{endpoint_id}/{funct}."
+
+            payload = {
+                "MODULENAME": function.config.module_name,
+                "CLASSNAME": function.config.class_name,
+                "funct": function.function,
+                "setting": json.dumps(setting),
+                "params": json.dumps(params),
+                "body": body,
+            }
+            res = LambdaBase.invoke(
+                function.aws_lambda_arn,
+                payload,
+                invocation_type=function.config.funct_type,
+            )
+            return {
+                "statusCode": 500
+                if funct.find("graphql") != -1
+                and "errors" in Utility.json_loads(res).keys()
+                else 200,
+                "headers": {
+                    "Access-Control-Allow-Headers": "Access-Control-Allow-Origin",
+                    "Access-Control-Allow-Origin": "*",
+                },
+                "body": res,
+            }
+
+        except Exception:
+            log = traceback.format_exc()
+            self.logger.exception(log)
+            return {
+                "statusCode": 500,
+                "headers": {
+                    "Access-Control-Allow-Headers": "Access-Control-Allow-Origin",
+                    "Access-Control-Allow-Origin": "*",
+                },
+                "body": (json.dumps({"error": log}, indent=4)),
+            }
