587e33ee4492ab3744cb7a8250204348f4e9c9fb
Barry Liu <barry.y.liu@hotmail.com>
2022-03-02 16:46:57 +0800

    [U] Update

--- silvaengine_base/__init__.py
+++ silvaengine_base/__init__.py
@@ -2,9 +2,10 @@
 # -*- coding: utf-8 -*-
 __author__ = "bibow"
 
-__all__ = ["resources", "tasks", "worker", "models"]
+__all__ = ["resources", "tasks", "worker", "models", "lambdabase"]
 
 from .resources import Resources
 from .tasks import Tasks
 from .worker import Worker
 from .models import *
+from .lambdabase import LambdaBase
