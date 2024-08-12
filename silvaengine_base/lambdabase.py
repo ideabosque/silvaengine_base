0ab1ff9c546b5d843d587689067fe5f73858d066
Barry Liu <barry.y.liu@hotmail.com>
2024-08-12 12:29:24 +0800

    [U] Update

--- silvaengine_base/lambdabase.py
+++ silvaengine_base/lambdabase.py
@@ -5,7 +5,6 @@ __author__ = "bibow"
 
 import json, boto3, os
 from boto3.dynamodb.conditions import Key
-from datetime import datetime, timezone
 from .models import EndpointsModel, ConnectionsModel, FunctionsModel, HooksModel
 
 
@@ -33,7 +32,6 @@ class LambdaBase(object):
 
     @classmethod
     def invoke(cls, function_name, payload, invocation_type="Event"):
-        print(">>> SILVA ENGINE > LABMDABASE > INVOKE at ", datetime.now(timezone.utc).isoformat())
         response = cls.aws_lambda.invoke(
             FunctionName=function_name,
             InvocationType=invocation_type,
@@ -42,12 +40,9 @@ class LambdaBase(object):
 
         if "FunctionError" in response.keys():
             log = json.loads(response["Payload"].read())
-            print("<<< SILVA ENGINE > LABMDABASE > INVOKE at ", datetime.now(timezone.utc).isoformat())
             raise FunctionError(log)
         if invocation_type == "RequestResponse":
-            print("<<< SILVA ENGINE > LABMDABASE > V at ", datetime.now(timezone.utc).isoformat())
             return json.loads(response["Payload"].read())
-        print("<<< SILVA ENGINE > LABMDABASE > INVOKE at ", datetime.now(timezone.utc).isoformat())
 
     @classmethod
     def get_hooks(cls, api_id) -> list:
