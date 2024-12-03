c5bd45f525adaece0f3c10db24d6be56c03320f5
Barry Liu <barry.y.liu@hotmail.com>
2024-12-03 15:53:48 +0800

    [U] Update

--- silvaengine_base/resources.py
+++ silvaengine_base/resources.py
@@ -54,13 +54,9 @@ class Resources(LambdaBase):
         """
         if route_key == "$connect":
             self.logger.info(f"WebSocket connected: {connection_id}")
-
-            # if event.get("requestContext", {}).get("authorizer") is not None:
-            #     return {"statusCode": 200, "body": "Connection successful"}
-            result = {"statusCode": 200, "body": "Connection successful"}
-
+            
             if self._is_request_event(event):
-                result = self._dynamic_authorization(event, context, "authorize")
+                return self._dynamic_authorization(event, context, "authorize")
 
             endpoint_id = event.get("queryStringParameters", {}).get("endpointId")
             area = event.get("queryStringParameters", {}).get("area", "")
@@ -70,7 +66,7 @@ class Resources(LambdaBase):
                 if api_key is None
                 else api_key
             )
-            
+
             if api_key is not None and endpoint_id is not None:
                 WSSConnectionModel(
                     endpoint_id,
@@ -78,13 +74,13 @@ class Resources(LambdaBase):
                     **{
                         "api_key": api_key,
                         "area": area,
-                        "data": result.get("context",{}),
+                        "data": event.get("requestContext", {}).get("authorizer",{}),
                         "updated_at": pendulum.now("UTC"),
                         "created_at": pendulum.now("UTC"),
                     },
                 ).save()
 
-            return result
+            return {"statusCode": 200, "body": "Connection successful"}
 
         elif route_key == "$disconnect":
             self.logger.info(f"WebSocket disconnected: {connection_id}")
