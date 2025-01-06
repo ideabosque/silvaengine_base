69f23ea571cfd1d843c6cf6009a7d69d1fb30ab6
Jeffrey Wang <azrael_wang@aliyun.com>
2025-01-06 11:48:46 +0800

    add event in _invoke_function

--- silvaengine_base/resources.py
+++ silvaengine_base/resources.py
@@ -360,6 +360,7 @@ class Resources(LambdaBase):
             "context": jsonpickle.encode(
                 event.get("requestContext"), unpicklable=False
             ),
+            "event": jsonpickle.encode(event, unpicklable=False)
         }
 
         if function.config.funct_type.strip().lower() == "event":
