af1b9339b587c380cc8209553db94a3b16e19c5d
Barry Liu <barry.y.liu@gmail.com>
2025-12-04 17:58:42 +0800

    [U] Debug.

--- silvaengine_base/resources.py
+++ silvaengine_base/resources.py
@@ -449,7 +449,9 @@ class Resources(LambdaBase):
 
         r = Utility.json_loads(result)
 
-        self.logger.info(f"Process response {r.get("status_code", 200)} with data {r.get("data", {})}")
+        self.logger.info(f"Process response {r.get("status_code", 200)} with data {r.get("data")}")
+        # self.logger.info(f"Process response {r.get("status_code", 200)} with data {r.get("data")}")
+
 
         return self._generate_response(r.get("status_code", 200), r.get("data", {}))
 
