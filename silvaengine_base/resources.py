f9fbdf4aeb19e7ba84e270c3d622dca4274f9b8b
Barry Liu <barry.y.liu@hotmail.com>
2025-12-05 11:20:49 +0800

    [U] Debug.

--- silvaengine_base/resources.py
+++ silvaengine_base/resources.py
@@ -405,7 +405,7 @@ class Resources(LambdaBase):
         }
 
         if event.get("body"):
-            payload.update({"body": event.get("body")})
+            payload.update(event.get("body"))
         # self.logger.info(f"Invoking function >>>>>> {payload}")
         # self.logger.info(f"Invoking function >>>>>> type is {type(payload)}")
         # self.logger.info(f"Invoking function {setting}")
