3eaa92b7584cd730c9a747bbc2c62bf3ece464b9
Barry Liu <barry.y.liu@hotmail.com>
2025-12-06 01:05:44 +0800

    [U] Add debug info.

--- silvaengine_base/lambdabase.py
+++ silvaengine_base/lambdabase.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 __author__ = "bibow"
 
-import boto3
-import os
-from typing import Any
+import boto3, os, pendulum
+from typing import Any, Dict, Tuple, List
 from silvaengine_utility import Utility
+from .models import EndpointModel, ConnectionModel, FunctionModel, HookModel, ConfigModel, WSSConnectionModel
 
 
 class FunctionError(Exception):
@@ -78,3 +78,159 @@ class LambdaBase:
             if isinstance(e, FunctionError):
                 raise e
             raise FunctionError(f"Failed to invoke Lambda function: {str(e)}")
+
+    @classmethod
+    def get_hooks(cls, api_id: str) -> List[Dict[str, Any]]:
+        """
+        Fetch active hooks for a given API ID.
+        :param api_id: The ID of the API.
+        :return: A list of hooks.
+        """
+        if not api_id:
+            return {}
+        
+        try:
+            return [
+                {item.variable: item.value}
+                for item in HookModel.query(api_id, None, HookModel.status.is_(True))
+            ]
+        except Exception as e:
+            if isinstance(e, ValueError):
+                raise e
+            raise ValueError(f"Failed to get hooks {api_id}: {str(e)}")
+
+    @classmethod
+    def get_setting(cls, setting_id: str) -> Dict[str, Any]:
+        """
+        Fetch a setting from DynamoDB based on the setting ID with caching.
+        :param setting_id: The ID of the setting.
+        :return: A dictionary of settings.
+        """
+        if not setting_id:
+            return {}
+        
+        try:
+            return {item.variable: item.value for item in ConfigModel.query(setting_id)}
+        except Exception as e:
+            if isinstance(e, ValueError):
+                raise e
+            raise ValueError(f"Failed to get setting {setting_id}: {str(e)}")
+
+    @classmethod
+    def get_function(
+        cls, endpoint_id: str, function_name: str, api_key: str = "#####", method: str = None
+    ) -> Tuple[Dict[str, Any], FunctionModel]:
+        """
+        Fetch the function configuration for a given endpoint.
+        :param endpoint_id: ID of the endpoint.
+        :param function_name: Name of the function to retrieve.
+        :param api_key: The API key, default is "#####".
+        :param method: The HTTP method if applicable.
+        :return: A tuple containing the merged settings and the function object.
+        """
+        endpoint = EndpointModel.get(endpoint_id) if endpoint_id != "0" else None
+        endpoint_id = endpoint_id if (endpoint and endpoint.special_connection) else "1"
+        connection = ConnectionModel.get(endpoint_id, api_key)
+        functions = [f for f in connection.functions if f.function == function_name]
+
+        if not functions:
+            raise ValueError(
+                f"Cannot find the function({function_name}) with endpoint_id({endpoint_id}) and api_key({api_key})."
+            )
+
+        function = FunctionModel.get(functions[0].aws_lambda_arn, functions[0].function)
+
+        if function is None:
+            raise ValueError(
+                "Cannot locate the function!! Please check the path and parameters."
+            )
+
+        # Merge settings from connection and function, connection settings override function settings
+        setting = {
+            **cls.get_setting(function.config.setting),
+            **(cls.get_setting(function.setting) if function.setting else {}),
+        }
+
+        if method and method not in function.config.methods:
+            raise ValueError(
+                f"The function({function_name}) doesn't support the method({method})."
+            )
+
+        return setting, function
+
+    @classmethod
+    def save_wss_connection(cls, endpoint_id: str, connection_id: str, api_key: str, area: str, data: dict) -> Dict[str, Any] | None:
+        """
+        Save a WSS connection model to DynamoDB.
+        :param endpoint_id: The ID of the endpoint.
+        :param connection_id: The ID of the connection.
+        :param api_key: The API key.
+        :param area: The area.
+        :param data: The connection data.
+        """
+        try:
+            return WSSConnectionModel(
+                endpoint_id,
+                connection_id,
+                **{
+                    "api_key": api_key,
+                    "area": area,
+                    "data": data,
+                    "updated_at": pendulum.now("UTC"),
+                    "created_at": pendulum.now("UTC"),
+                },
+            ).save()
+        except Exception as e:
+            raise ValueError(f"Failed to save WSS connection: {str(e)}")
+        
+    @classmethod
+    def get_wss_connection(cls, endpoint_id: str, connection_id: str) -> WSSConnectionModel:
+        """
+        Get a WSS connection model from DynamoDB.
+        :param endpoint_id: The ID of the endpoint.
+        :param connection_id: The ID of the connection.
+        :return: The WSS connection model.
+        """
+        try:
+            return WSSConnectionModel.get(endpoint_id, connection_id)
+        except Exception as e:
+            raise ValueError(f"Failed to get WSS connection: {str(e)}")
+        
+    @classmethod
+    def get_wss_connections(cls, connection_id: str) -> List[WSSConnectionModel]:
+        """
+        Get all WSS connection models from DynamoDB for a given connection.
+        :param connection_id: The ID of the connection.
+        :return: A list of WSS connection models.
+        """
+        try:
+            return WSSConnectionModel.connect_id_index.query(connection_id)
+        except Exception as e:
+            raise ValueError(f"Failed to get WSS connections: {str(e)}")
+        
+    @classmethod
+    def remove_expired_connections(cls, endpoint_id: str, email: str) -> None:
+        """
+        Get all WSS connection models from DynamoDB for a given endpoint.
+        :param endpoint_id: The ID of the endpoint.
+        :param range_condition: The range key condition.
+        :param cutoff_time: The cutoff time.
+        :return: A list of WSS connection models.
+        """
+        try:
+            connections = WSSConnectionModel.query(
+                hash_key=endpoint_id,
+                filter_condition=WSSConnectionModel.updated_at < pendulum.now("UTC").subtract(days=1),
+            )
+
+            # Iterate through and delete matching connections
+            for connection in connections:
+                if (
+                    email is not None
+                    and connection.data.__dict__["attribute_values"].get("email") != email
+                ):
+                    pass
+
+                connection.delete()
+        except Exception as e:
+            raise ValueError(f"Failed to get WSS connections: {str(e)}")
