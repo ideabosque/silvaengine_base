7f67e7766c7ba379784c23cb683dc482c91fdd8a
Barry Liu <barry.y.liu@hotmail.com>
2025-12-05 16:01:55 +0800

    [U] Add debug info.

--- silvaengine_base/resources.py
+++ silvaengine_base/resources.py
@@ -381,8 +381,8 @@ class Resources(LambdaBase):
             if len(exception.args) > 1 and isinstance(exception.args[1], int):
                 status_code = exception.args[1]
 
-        if self._is_request_event(event):
-            return self._handle_authorizer_failure(event, str(message))
+        # if self._is_request_event(event):
+        #     return self._handle_authorizer_failure(event, str(message))
 
         return self._generate_response(status_code, str(message))
     
