9de798b4e7c4e4e984c4cfade4ad70009906c2db
Barry Liu <barry.y.liu@hotmail.com>
2026-01-14 13:44:26 +0800

    [U] Optimization

--- silvaengine_base/handlers/websocket.py
+++ silvaengine_base/handlers/websocket.py
@@ -147,9 +147,10 @@ class WebSocketHandler(Handler):
                 )
 
                 if wss_connection:
-                    wss_connection.status = SwitchStatus.INACTIVE.name
-                    wss_connection.updated_at = pendulum.now("UTC")
-                    wss_connection.save()
+                    wss_connection.delete()
+                    # wss_connection.status = SwitchStatus.INACTIVE.name
+                    # wss_connection.updated_at = pendulum.now("UTC")
+                    # wss_connection.save()
             except Exception as e:
                 Debugger.info(
                     variable=e,
