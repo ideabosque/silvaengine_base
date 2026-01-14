abcfee9d85d961fd333ec96b46019262ef7c432f
Barry Liu <barry.y.liu@hotmail.com>
2026-01-14 16:57:25 +0800

    [U] Optimization

--- silvaengine_base/handlers/websocket.py
+++ silvaengine_base/handlers/websocket.py
@@ -70,7 +70,6 @@ class WebSocketHandler(Handler):
                 delimiter="#",
                 logger=self.logger,
             )
-            print(traceback.format_exc())
             return self._generate_response(
                 status_code=HttpStatus.OK.value,
                 body={"data": str(e)},
@@ -164,12 +163,6 @@ class WebSocketHandler(Handler):
         elif route_key == "stream":
             return self._message()
 
-        Debugger.info(
-            variable="Invalid websocket route",
-            stage="WEBSOCKET TEST",
-            delimiter="#",
-            logger=self.logger,
-        )
         return self._generate_response(
             status_code=HttpStatus.OK.value,
             body={"data": "Invalid websocket route"},
@@ -184,12 +177,6 @@ class WebSocketHandler(Handler):
             connection_id = self._get_connection_id()
 
             if not connection_id:
-                Debugger.info(
-                    variable="Invalid websocket connection",
-                    stage="WEBSOCKET TEST",
-                    delimiter="#",
-                    logger=self.logger,
-                )
                 return self._generate_response(
                     status_code=HttpStatus.OK.value,
                     body={"data": "Invalid websocket connection id"},
@@ -199,12 +186,6 @@ class WebSocketHandler(Handler):
             endpoint_id = self._get_endpoint_id()
 
             if not endpoint_id:
-                Debugger.info(
-                    variable="Invalid websocket connection endpoint id",
-                    stage="WEBSOCKET TEST",
-                    delimiter="#",
-                    logger=self.logger,
-                )
                 return self._generate_response(
                     status_code=HttpStatus.OK.value,
                     body={"data": "Invalid websocket connection endpoint id"},
@@ -215,12 +196,6 @@ class WebSocketHandler(Handler):
             function = body.get("funct")
 
             if not function:
-                Debugger.info(
-                    variable="Missing required `funct`",
-                    stage="WEBSOCKET TEST",
-                    delimiter="#",
-                    logger=self.logger,
-                )
                 return self._generate_response(
                     status_code=HttpStatus.OK.value,
                     as_websocket_format=True,
@@ -243,12 +218,6 @@ class WebSocketHandler(Handler):
 
             self._merge_setting_to_default(setting=setting)
 
-            Debugger.info(
-                variable=f"Endpoint ID: {endpoint_id}, Connection ID: {connection_id}",
-                stage="WEBSOCKET DEBUG",
-                delimiter="#",
-                logger=self.logger,
-            )
             wss_connection = self._get_current_connection(
                 endpoint_id=endpoint_id,
                 connection_id=connection_id,
@@ -277,38 +246,16 @@ class WebSocketHandler(Handler):
                 or not hasattr(function.config, "class_name")
                 or not hasattr(function, "function")
             ):
-                Debugger.info(
-                    variable="Invalid function",
-                    stage="WEBSOCKET TEST",
-                    delimiter="#",
-                    logger=self.logger,
-                )
                 return self._generate_response(
                     status_code=HttpStatus.OK.value,
                     as_websocket_format=True,
                     body={"data": "Invalid function"},
                 )
 
-            Debugger.info(
-                variable=parameters,
-                stage="WEBSOCKET TEST",
-                delimiter="#",
-                logger=self.logger,
-            )
-
-            r = self._get_proxied_callable(
+            return self._get_proxied_callable(
                 module_name=function.config.module_name,
                 class_name=function.config.class_name,
                 function_name=function.function,
             )(**parameters)
-
-            Debugger.info(
-                variable=parameters,
-                stage="WEBSOCKET RESPONSE",
-                delimiter="+",
-                logger=self.logger,
-            )
-
-            return r
         except Exception as e:
             raise e
