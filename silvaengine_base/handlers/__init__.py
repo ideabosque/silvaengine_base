b82083cea86a71241db7a2b54ab592dd3917456e
Barry Liu <barry.y.liu@hotmail.com>
2026-01-24 18:23:01 +0800

    [U] Optimization

--- silvaengine_base/handlers/__init__.py
+++ silvaengine_base/handlers/__init__.py
@@ -4,13 +4,13 @@ from __future__ import print_function
 
 from .cloudwatch_log import CloudwatchLogHandler
 from .cognito import CognitoHandler
+from .default import DefaultHandler
 from .dynamodb import DynamodbHandler
 from .event_bridge import EventBridgeHandler
 from .http import HttpHandler
 from .s3 import S3Handler
 from .sns import SNSHandler
 from .sqs import SQSHandler
-from .unknown import UnknownHandler
 from .websocket import WebSocketHandler
 
 __all__ = [
@@ -23,5 +23,5 @@ __all__ = [
     "SNSHandler",
     "SQSHandler",
     "WebSocketHandler",
-    "UnknownHandler",
+    "DefaultHandler",
 ]
